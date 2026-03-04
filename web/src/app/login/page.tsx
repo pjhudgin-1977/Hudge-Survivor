bb169cc554c550006c9bf9155d60b0c579cadf92
Patrick Hudgin <patrickhudgin@Patricks-MacBook-Air.local>
2026-03-03 21:58:29 -0800

    Fix: disable SSR for /login to avoid prerender build error

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -1,4 +1,6 @@
-import LoginClient from "./LoginClient";
+import dynamic from "next/dynamic";
+
+const LoginClient = dynamic(() => import("./LoginClient"), { ssr: false });
 
 export default function LoginPage() {
   return <LoginClient />;
